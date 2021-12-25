a888f006984b96ef446cbbc5d5e39bcc83502197
David Whitman <whitmadm@gmail.com>
2021-12-25 10:48:58 -0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.5.31"
+    id("org.jetbrains.compose") version "1.0.0"
+}
+
+group = "com.wisp"
+version = "1.0"
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+
+dependencies {
+    implementation(compose.desktop.currentOs)
+
+    implementation("org.update4j:update4j:1.5.8")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "16"
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "SelfUpdateTesting"
+            packageVersion = "1.0.0"
+        }
+    }
+}
