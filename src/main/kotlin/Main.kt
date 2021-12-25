9889fe185a835e2048baa2cf6d9bdf01cde932f8
David Whitman <whitmadm@gmail.com>
2021-12-25 13:00:39 -0800

    Adding update-config xml.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,21 @@
 // Copyright 2000-2021 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
-import androidx.compose.material.MaterialTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import org.update4j.Configuration
+import org.update4j.FileMetadata
+import org.update4j.UpdateOptions
+import java.net.URI
+import java.nio.file.Path
+import java.nio.file.Paths
+import kotlin.io.path.writer
+import kotlin.streams.asSequence
+
+val baseUrl = "https://raw.githubusercontent.com/davidwhitman/SelfUpdateTesting/master"
 
 @Composable
 @Preview
@@ -27,11 +32,43 @@ fun App() {
 }
 
 fun main() = application {
-    val config = Configuration.builder()
-        .baseUri
-
+    writeLocalUpdateConfig()
+    updateApp()
 
     Window(onCloseRequest = ::exitApplication) {
         App()
     }
+}
+
+private fun updateApp() {
+    val remoteConfigUrl = URI.create("$baseUrl/update-config.xml").toURL()
+
+    val remoteConfig = runCatching {
+        remoteConfigUrl.openStream().use { stream ->
+            Configuration.read(stream.bufferedReader())
+        }
+    }
+        .onFailure { println(it) }
+        .getOrNull()
+
+    remoteConfig?.update(UpdateOptions.archive(Path.of("update.zip")))
+}
+
+private fun writeLocalUpdateConfig(): Configuration? {
+    val config = Configuration.builder()
+        .baseUri(baseUrl)
+        .basePath(Paths.get("").toAbsolutePath().toString())
+        .files(
+            FileMetadata.streamDirectory("build\\compose\\binaries\\main\\app\\SelfUpdateTesting")
+                .asSequence()
+                .onEach { r -> r.classpath(r.source.toString().endsWith(".jar")) }
+                .toList())
+        .build()
+
+
+    Path.of("update-config.xml").writer().use {
+        config.write(it)
+        println("Wrote config to $it")
+    }
+    return config
 }
